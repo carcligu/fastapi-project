dd2c3c8a6cfa8c1f43b0c167f03c9b3a041f818c
carcligu <carcligu@gmail.com>
2021-06-08 18:24:26 +0200

    Path parameters introduction

--- main.py
+++ main.py
@@ -5,4 +5,16 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"data": "project list"}
+
+
+@app.get('/project/{id}')
+async def show(id: int):
+    # fetch project with id = id
+    return {"data": id}
+
+
+@app.get('/project/{id}/comments')
+async def comments(id: int):
+    # fetch comments of project with id = id
+    return {'data': {'1', '2'}}
