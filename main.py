c052064db80d9c51f5adf842122f56a92e0deda8
carcligu <carcligu@gmail.com>
2021-06-08 18:35:37 +0200

    Query parameters introduction

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
@@ -8,13 +9,22 @@ async def root():
     return {"data": "project list"}
 
 
+@app.get("/project")
+async def all_projects(limit=10, sort: Optional[str] = None):
+    # fletch all projects. limit is used to not pull all data, so it is executed faster
+    # the query parameter can be introduced in the path:
+    #   /project?limit=10
+    return {'data': f'{limit} projects from the db'}
+
+
 @app.get('/project/{id}')
 async def show(id: int):
     # fetch project with id = id
     return {"data": id}
 
 
-@app.get('/project/{id}/comments')
+
+@app.get('/project/{id}/technologies')
 async def comments(id: int):
     # fetch comments of project with id = id
     return {'data': {'1', '2'}}
