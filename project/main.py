84b548f4cda1ed5e817b927cabe22f1fa08f36a4
carcligu <carcligu@gmail.com>
2021-06-09 11:10:05 +0200

    Query data from database

--- project/main.py
+++ project/main.py
@@ -1,9 +1,12 @@
+from main import Project
 from fastapi import FastAPI, Depends
 from sqlalchemy import engine
 from . import schemas
 from . import models
 from . import database
 from sqlalchemy.orm import Session
+
+import project
 
 app = FastAPI()
 
@@ -17,6 +20,7 @@ def get_db():
     finally:
         db.close()
 
+
 @app.post('/project')
 def create(request: schemas.Project, db: Session = Depends(get_db)):
     new_project = models.Project(
@@ -27,3 +31,15 @@ def create(request: schemas.Project, db: Session = Depends(get_db)):
     db.commit()
     db.refresh
     return new_project
+
+
+@app.get('/project')
+def all_projects(db: Session = Depends(get_db)):
+    projects = db.query(models.Project).all()
+    return projects
+
+
+@app.get('/project/{id}')
+def get_project(id, db: Session = Depends(get_db)):
+    project = db.query(models.Project).filter(models.Project.id == id).first()
+    return project
