3f8ab4e3410672e899b49bafa66af2de8ecf7079
carcligu <carcligu@gmail.com>
2021-06-09 10:34:00 +0200

    Storing data into database

--- project/main.py
+++ project/main.py
@@ -1,14 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from sqlalchemy import engine
 from . import schemas
 from . import models
 from . import database
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
 
 models.Project.metadata.create_all(database.engine)
 
+def get_db():
+    db = database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 @app.post('/project')
-def create(request: schemas.Project):
-    return {'title': request.title, 'description': request.description}
+def create(request: schemas.Project, db: Session = Depends(get_db)):
+    new_project = models.Project(
+                        title=request.title,
+                        description=request.description
+                    )
+    db.add(new_project)
+    db.commit()
+    db.refresh
+    return new_project
